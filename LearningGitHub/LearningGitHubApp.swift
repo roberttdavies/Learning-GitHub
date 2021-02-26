02a2e1bc23efeeafb3c0f07e3152bc4660c3a9db
Bob Davies <roberttdavies@gmail.com>
2021-02-26 16:06:01 +0000

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Bob Davies on 26/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
